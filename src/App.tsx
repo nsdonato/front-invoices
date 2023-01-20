499ed0530a56e5b8aa5461ec50a199fc52db814f
Ch3CoX <checox.ss7@gmail.com>
2023-01-19 21:26:14 -0400

    feat: :sparkles: actualizo los nombres de los directorios

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Button from "./components/button/Button";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
--- src/tests/button/button.test.tsx
+++ src/tests/button/button.test.tsx
@@ -1,4 +1,4 @@
-import Button from "../../components/button/Button";
+import Button from "../../components/Button/Button";
 import { render, screen } from "../../utils/test-utils";
 
 describe("Button display", () => {

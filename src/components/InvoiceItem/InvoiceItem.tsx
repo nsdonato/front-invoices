859ad9ff3bb85db9d5277ddd09a6e633f26a6963
AllegriM <elmonky99@gmail.com>
2023-01-27 00:09:45 -0300

    fix: :lipstick: update and fix status colors on dark mode and light & amount text color

--- src/components/InvoiceItem/InvoiceItem.tsx
+++ src/components/InvoiceItem/InvoiceItem.tsx
@@ -15,13 +15,13 @@ type InvoiceItemProps<C extends ElementType> = {
 const invoiceBackgrounds: Record<Status, string> = {
   paid: "bg-brand-green-ghost",
   pending: "bg-brand-orange-ghost",
-  draft: "bg-brand-violet-ghost",
+  draft: "bg-brand-gray-ghost",
 };
 
 const invoiceDots: Record<Status, string> = {
   paid: "before:bg-brand-green",
   pending: "before:bg-brand-orange",
-  draft: "before:bg-brand-violet-muted",
+  draft: "before:bg-brand-gray-muted",
 };
 
 const invoiceTexts: Record<Status, string> = {
@@ -75,7 +75,7 @@ export const InvoiceItem = <C extends ElementType>({
   return (
     <MutableElement
       {...props}
-      className="relative bg-white lg:max-w-3xl md:max-w-2xl max-w-sm py-4 lg:pr-6 md:pr-6 rounded-lg mx-auto my-4 dark:bg-brand-gray-dark grid md:grid-rows-1 lg:grid-rows-1 lg:grid-cols-5 md:grid-cols-5 grid-cols-2 items-center text-center shadow-md cursor-pointer text-sm border transition-all hover:border-brand-violet"
+      className="relative bg-brand bg-white lg:max-w-3xl md:max-w-2xl max-w-sm py-4 lg:pr-6 md:pr-6 rounded-lg mx-auto my-4 dark:bg-brand-gray-dark grid md:grid-rows-1 lg:grid-rows-1 lg:grid-cols-5 md:grid-cols-5 grid-cols-2 items-center text-center shadow-md cursor-pointer text-sm border transition-all hover:border-brand-violet"
     >
       <p className="font-bold text-brand-text dark:text-white lg:order-1 md:order-1 order-0 lg:mb-0 md:mb-0 mb-3">
         <span className="text-brand-violet-muted ">#</span>
@@ -87,20 +87,28 @@ export const InvoiceItem = <C extends ElementType>({
       <p className="text-brand-text-light dark:text-white lg:order-3 md:order-3 order-1 lg:mb-0 md:mb-0 mb-3">
         {name}
       </p>
-      <h4 className="font-bold text-base lg:order-4 md:order-4 order-4 ">
+      <h4 className="font-bold text-base lg:order-4 md:order-4 order-4 dark:text-brand-white">
         {normalizedAmount}
       </h4>
       <div
-        className={`${invoiceBackgrounds[status]} max-w-[104px] mx-auto w-full relative py-3 rounded-md lg:order-5 md:order-5 order-3 row-span-2`}
+        className={`${invoiceBackgrounds[status]} ${
+          status === "draft" ? "dark:bg-brand-violet-highlight" : ""
+        } dark:bg-opacity-5 max-w-[104px] mx-auto w-full relative py-3 rounded-md lg:order-5 md:order-5 order-3 row-span-2`}
       >
-        <p className={`${invoiceTexts[status]} relative font-bold`}>
+        <p
+          className={`${invoiceTexts[status]} ${
+            status === "draft" ? "dark:text-brand-white" : ""
+          } relative font-bold`}
+        >
           <span
-            className={`absolute top-1 before:absolute ${invoiceDots[status]} before:rounded-full before:p-[3px] before:right-1`}
+            className={`absolute top-1 before:absolute ${invoiceDots[status]} ${
+              status === "draft" ? "dark:before:bg-brand-white" : ""
+            } before:rounded-full before:p-[3px] before:right-1`}
           ></span>
           {normalizedStatus}
         </p>
       </div>
-      <span className="ml-2 w-fit right-5 absolute lg:block lg:order-6 md:block md:order-6 hidden">
+      <span className="ml-2 w-fit right-4 absolute lg:block lg:order-6 md:block md:order-6 hidden">
         <Arrow />
       </span>
     </MutableElement>

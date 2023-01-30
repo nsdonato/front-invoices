7122eb8ba64a68d022d05b1b35752ef9430e8d6d
Noelia Donato <noeliadonato@gmail.com>
2023-01-29 21:39:54 -0300

    feat(test): fix test post merge

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,12 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <>
-
       <NavBar />
       <h1 className="text-8xl font-bold underline decoration-sky-600 hover:decoration-blue-400">
         Invoice App
-        </h1>
-        <Outlet />
-   </>
+      </h1>
+      <Outlet />
+    </>
   );
 }
 

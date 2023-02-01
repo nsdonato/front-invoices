16db317f7bcd619244e8383e5634240b5ab51634
Mateo2131 <goz.matu@gmail.com>
2023-02-01 17:50:35 -0300

    fix: add darkmode button

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,8 +1,10 @@
+import DarkMode from "../Darkmode";
+
 const NavBar = () => {
   return (
     <nav
       data-testid="nav"
-      className="flex items-center justify-between w-screen bg-brand-gray darkMode:bg-brand-gray-dark brand-lg:flex-col brand-lg:w-[103px] brand-lg:h-screen brand-lg:rounded-r-[20px] "
+      className="flex items-center justify-between w-screen bg-brand-gray darkMode:bg-brand-gray-dark brand-lg:flex-col brand-lg:w-[103px] brand-lg:h-screen brand-lg:rounded-r-[20px]"
     >
       <span>
         <img
@@ -11,10 +13,13 @@ const NavBar = () => {
           alt="logo-invoice-app"
         />
       </span>
-      <div className="border-brand-gray-light px-[24px] py-[20px] border-l-[1px] brand-lg:py-[24px] brand-lg:px-[31px] brand-lg:border-0 brand-lg:border-t-[1px]">
-        <span className="flex brand-lg:w-[40px] brand-lg:h-[40px] w-[32px] h-[32px] p-1.5 overflow-hidden bg-gray-200 rounded-full ">
-          <img src="/avatar.svg" alt="avatar-skeleton" />
-        </span>
+      <div className="flex items-center brand-lg:flex-col gap-4">
+        <DarkMode />
+        <div className="border-brand-gray-light px-[24px] py-[20px] border-l-[1px] brand-lg:py-[24px] brand-lg:px-[31px] brand-lg:border-0 brand-lg:border-t-[1px]">
+          <span className="flex brand-lg:w-[40px] brand-lg:h-[40px] w-[32px] h-[32px] p-1.5 overflow-hidden bg-gray-200 rounded-full ">
+            <img src="/avatar.svg" alt="avatar-skeleton" />
+          </span>
+        </div>
       </div>
     </nav>
   );
